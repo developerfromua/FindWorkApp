485b33cdf647f894c5c12425830387cd54c86159
home <rendyhardy46@gmail.com>
2022-02-25 00:23:30 +0300

    firebase launch

--- app/src/main/java/com/rsoftware/findworkapp/LoginActivity.java
+++ app/src/main/java/com/rsoftware/findworkapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.rsoftware.findworkapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 }
